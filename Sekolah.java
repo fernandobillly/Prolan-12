88d84f25b5b6860e046b6cd146fe0971ba6a8361
fernandobillly <nandotorez@ymail.com>
2017-03-07 23:50:14 +0700

    Sekolah
    Class dan Atribut

---
+++ Sekolah.java
@@ -0,0 +1,14 @@
+public class Sekolah {
+	Public string bangunan, pintu, jendela, papantulis, meja, kursi, kamarmandi;
+
+	void bagianrumah ()
+	   {
+	    System.Outprinln ("bangunan");
+		System.Outprinln ("pintu");
+		System.Outprinln ("jendela");
+		System.Outprinln ("papantulis");
+		System.Outprinln ("meja");
+		System.Outprinln ("kursi");
+		System.Outprinln ("kamarmandi");
+	   }
+}
