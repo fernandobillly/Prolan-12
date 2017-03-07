adf76dabb83e8770907570f574f7d9470684f265
fernandobillly <nandotorez@ymail.com>
2017-03-07 23:37:33 +0700

    Rumah
    Menganalisa Class dan Atribut

---
+++ Rumah.java
@@ -0,0 +1,13 @@
+class Rumah {
+	Public string bangunan, pintu, jendela, kamartidur, kamarmandi, ruangtamu;
+
+	void bagianrumah ()
+	   {
+	    System.Outprinln ("bangunan");
+		System.Outprinln ("pintu");
+		System.Outprinln ("jendela");
+		System.Outprinln ("kamartidur");
+		System.Outprinln ("kamarmandi");
+		System.Outprinln ("ruangtamu");
+	   }
+}
